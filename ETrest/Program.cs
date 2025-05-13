ae3ab31bf68635c313122ac5237b016e5844e478
sebastianhansen <seh001@edu.zealand.dk>
2025-05-13 13:18:09 +0200

    Cors tilføjet

--- ETrest/Program.cs
+++ ETrest/Program.cs
@@ -2,21 +2,19 @@ using ETlib.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        });
+});
+
 // Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-    });
-});
-
 
 builder.Services.AddSingleton<EnergyPriceRepository>(new EnergyPriceRepository());
 
@@ -25,9 +23,8 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseCors("AllowAll");
-
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
 
